5b6e482c56e632a1a55c6c1ad8d9422afb0f5355
Letícia Xavier Abreu <1553571@sga.pucminas.br>
2026-05-25 11:52:32 -0300

    Adição de novos elementos no JSON, e correção das páginas auxiliares

--- public/app.js
+++ public/app.js
@@ -88,7 +88,592 @@ let receitas = [
         estilo: "vegana",
         ocasiao: "dia a dia",
         tipo: "acompanhamento"
-    }
+    },
+
+    {
+    id: 7,
+    nome: "Torta de Frango",
+    imagem: "img/torta-frango.png",
+
+    receita: "Prepare a massa, recheie com frango desfiado temperado e asse até dourar.",
+
+    tempo: "demorado",
+    dificuldade: "média",
+    proteina: "frango",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "lanche"
+    },
+
+    {
+    id: 8,
+    nome: "Purê de Batata",
+    imagem: "img/pure.jpg",
+
+    receita: "Cozinhe as batatas, amasse bem e misture com manteiga, leite e sal até ficar cremoso.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "acompanhamento"
+    },
+
+    {
+    id: 9,
+    nome: "Moqueca de Peixe",
+    imagem: "img/moqueca.jpg",
+
+    receita: "Cozinhe o peixe com tomate, cebola, pimentão, leite de coco e azeite de dendê.",
+
+    tempo: "demorado",
+    dificuldade: "alta",
+    proteina: "peixe",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "almoço e jantar"
+    },
+
+    {
+    id: 10,
+    nome: "Panqueca Fitness",
+    imagem: "img/panqueca.jpg",
+
+    receita: "Prepare a massa com ovos e aveia, recheie com frango desfiado e molho leve.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "frango",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "lanche"
+    },
+
+    {
+    id: 11,
+    nome: "Bolo de Chocolate",
+    imagem: "img/bolo.jpg",
+
+    receita: "Misture os ingredientes da massa, asse até crescer e finalize com cobertura de brigadeiro.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+    },
+
+    {
+    id: 12,
+    nome: "Risoto de Carne",
+    imagem: "img/risoto.jpg",
+
+    receita: "Refogue a carne, adicione arroz arbóreo e caldo aos poucos até o risoto ficar cremoso.",
+
+    tempo: "demorado",
+    dificuldade: "alta",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 13,
+    nome: "Macarrão ao Molho Branco",
+    imagem: "img/macarrao.jpg",
+
+    receita: "Cozinhe o macarrão e prepare um molho branco com leite, manteiga e queijo. Misture tudo e sirva quente.",
+
+    tempo: "moderado",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 14,
+    nome: "Strogonoff de Frango",
+    imagem: "img/strogonoff.jpg",
+
+    receita: "Refogue o frango, adicione creme de leite e molho de tomate. Sirva com arroz e batata palha.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "frango",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 15,
+    nome: "Escondidinho de Carne",
+    imagem: "img/escondidinho.jpg",
+
+    receita: "Faça um purê de batatas e cubra um recheio de carne moída. Leve ao forno para gratinar.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 16,
+    nome: "Pizza Caseira",
+    imagem: "img/pizza.jpg",
+
+    receita: "Prepare a massa, adicione molho, queijo e recheios de sua preferência. Asse até dourar.",
+
+    tempo: "demorado",
+    dificuldade: "média",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "lanche"
+},
+
+{
+    id: 17,
+    nome: "Cheesecake de Morango",
+    imagem: "img/cheesecake.jpg",
+
+    receita: "Prepare a base de biscoito, o creme de queijo e finalize com calda de morango.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 18,
+    nome: "Wrap de Frango",
+    imagem: "img/wrap.jpg",
+
+    receita: "Recheie a massa com frango desfiado, alface e molho. Enrole e sirva.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "frango",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "lanche"
+},
+
+{
+    id: 19,
+    nome: "Arroz Carreteiro",
+    imagem: "img/carreteiro.jpg",
+
+    receita: "Refogue carne seca com alho e cebola, adicione arroz e cozinhe até ficar macio.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 20,
+    nome: "Mousse de Maracujá",
+    imagem: "img/mousse.jpg",
+
+    receita: "Bata leite condensado, creme de leite e suco de maracujá. Leve à geladeira.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 21,
+    nome: "Omelete Fit",
+    imagem: "img/omelete.jpg",
+
+    receita: "Bata ovos com legumes picados e prepare na frigideira antiaderente.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "frango",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "lanche"
+},
+
+{
+    id: 22,
+    nome: "Nhoque ao Sugo",
+    imagem: "img/nhoque.jpg",
+
+    receita: "Cozinhe os nhoques e cubra com molho de tomate caseiro e queijo ralado.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 23,
+    nome: "Cuscuz Nordestino",
+    imagem: "img/cuscuz.jpg",
+
+    receita: "Hidrate o flocão de milho e cozinhe no vapor. Sirva com manteiga ou ovos.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 24,
+    nome: "Pudim de Leite",
+    imagem: "img/pudim.jpg",
+
+    receita: "Misture leite condensado, leite e ovos. Asse em banho-maria até firmar.",
+
+    tempo: "demorado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 25,
+    nome: "Frango Xadrez",
+    imagem: "img/xadrez.jpg",
+
+    receita: "Refogue frango com legumes e molho shoyu. Sirva com arroz branco.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "frango",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 26,
+    nome: "Bruschetta Italiana",
+    imagem: "img/bruschetta.jpg",
+
+    receita: "Cubra fatias de pão com tomate, alho e manjericão. Leve ao forno rapidamente.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "vegana",
+    ocasiao: "festa",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 27,
+    nome: "Feijão Tropeiro",
+    imagem: "img/tropeiro.jpg",
+
+    receita: "Misture feijão, farinha, ovos, bacon e temperos. Refogue bem antes de servir.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 28,
+    nome: "Tapioca com Queijo",
+    imagem: "img/tapioca.jpg",
+
+    receita: "Espalhe a goma na frigideira, recheie com queijo e dobre ao meio.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "lanche"
+},
+
+{
+    id: 29,
+    nome: "Cupcake de Baunilha",
+    imagem: "img/cupcake.jpg",
+
+    receita: "Prepare a massa, asse em forminhas e finalize com cobertura doce.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 30,
+    nome: "Yakisoba",
+    imagem: "img/yakisoba.png",
+
+    receita: "Refogue legumes e carne, misture macarrão e molho shoyu.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 31,
+    nome: "Salpicão de Frango",
+    imagem: "img/salpicao.jpg",
+
+    receita: "Misture frango desfiado, legumes, maionese e batata palha.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "frango",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 32,
+    nome: "Torta Holandesa",
+    imagem: "img/torta.jpg",
+
+    receita: "Monte camadas de creme e biscoito, finalize com cobertura de chocolate.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 33,
+    nome: "Fricassê de Frango",
+    imagem: "img/fricasse.jpeg",
+
+    receita: "Misture frango desfiado com creme de milho, cubra com queijo e batata palha e leve ao forno.",
+
+    tempo: "moderado",
+    dificuldade: "média",
+    proteina: "frango",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 34,
+    nome: "Esfirra de Carne",
+    imagem: "img/esfirra.jpg",
+
+    receita: "Prepare a massa, recheie com carne temperada e asse até dourar.",
+
+    tempo: "demorado",
+    dificuldade: "média",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "lanche"
+},
+
+{
+    id: 35,
+    nome: "Muffin de Banana",
+    imagem: "img/muffin.jpg",
+
+    receita: "Misture banana amassada, farinha, ovos e açúcar. Asse em forminhas individuais.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "sobremesa"
+},
+
+{
+    id: 36,
+    nome: "Filé de Tilápia Grelhado",
+    imagem: "img/tilapia.jpg",
+
+    receita: "Tempere a tilápia com limão e ervas. Grelhe até dourar dos dois lados.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "peixe",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 37,
+    nome: "Batata Rústica",
+    imagem: "img/batata-rustica.jpg",
+
+    receita: "Corte as batatas em gomos, tempere com ervas e asse até ficarem crocantes.",
+
+    tempo: "moderado",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "vegana",
+    ocasiao: "dia a dia",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 38,
+    nome: "Empadão de Frango",
+    imagem: "img/empadao.jpg",
+
+    receita: "Prepare a massa amanteigada, recheie com frango cremoso e asse até dourar.",
+
+    tempo: "demorado",
+    dificuldade: "alta",
+    proteina: "frango",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "lanche"
+},
+
+{
+    id: 39,
+    nome: "Sorvete Caseiro",
+    imagem: "img/sorvete.jpg",
+
+    receita: "Misture creme de leite, leite condensado e saborizante. Leve ao congelador.",
+
+    tempo: "moderado",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 40,
+    nome: "Arroz à Grega",
+    imagem: "img/arroz-grega.jpg",
+
+    receita: "Misture arroz cozido com legumes coloridos e uvas-passas.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 41,
+    nome: "Crepioca Fit",
+    imagem: "img/crepioca.jpg",
+
+    receita: "Misture ovo e tapioca, recheie com queijo ou frango e prepare na frigideira.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "frango",
+    estilo: "fitness",
+    ocasiao: "dia a dia",
+    tipo: "lanche"
+},
+
+{
+    id: 42,
+    nome: "Canjica Doce",
+    imagem: "img/canjica.jpg",
+
+    receita: "Cozinhe a canjica com leite, açúcar e canela até ficar cremosa.",
+
+    tempo: "demorado",
+    dificuldade: "média",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "sobremesa"
+},
+
+{
+    id: 43,
+    nome: "Bobó de Camarão",
+    imagem: "img/bobo.jpg",
+
+    receita: "Cozinhe camarões com mandioca cremosa, leite de coco e azeite de dendê.",
+
+    tempo: "demorado",
+    dificuldade: "alta",
+    proteina: "peixe",
+    estilo: "normal",
+    ocasiao: "festa",
+    tipo: "almoço e jantar"
+},
+
+{
+    id: 44,
+    nome: "Farofa Completa",
+    imagem: "img/farofa.jpg",
+
+    receita: "Refogue farinha com bacon, ovos, cebola e temperos até dourar.",
+
+    tempo: "rápido",
+    dificuldade: "baixa",
+    proteina: "carne",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "acompanhamento"
+},
+
+{
+    id: 45,
+    nome: "Pão de Queijo",
+    imagem: "img/pao-de-queijo.jpg",
+
+    receita: "Misture polvilho, queijo, ovos e leite. Modele bolinhas e asse até crescer.",
+
+    tempo: "moderado",
+    dificuldade: "baixa",
+    proteina: "nenhuma",
+    estilo: "normal",
+    ocasiao: "dia a dia",
+    tipo: "lanche"
+}
 
 ];
 
@@ -96,7 +681,42 @@ const cards = document.getElementById("cards");
 
 if(cards){
 
-    receitas.forEach(receita => {
+    // pega o nome da página atual
+    const pagina = window.location.pathname;
+
+    let receitasFiltradas = receitas;
+
+    // filtros por página
+    if(pagina.includes("acompanhamentos.html")){
+
+        receitasFiltradas = receitas.filter(receita =>
+            receita.tipo == "acompanhamento"
+        );
+    }
+
+    else if(pagina.includes("lanches.html")){
+
+        receitasFiltradas = receitas.filter(receita =>
+            receita.tipo == "lanche"
+        );
+    }
+
+    else if(pagina.includes("sobremesas.html")){
+
+        receitasFiltradas = receitas.filter(receita =>
+            receita.tipo == "sobremesa"
+        );
+    }
+
+    else if(pagina.includes("almoco&jantar.html")){
+
+        receitasFiltradas = receitas.filter(receita =>
+            receita.tipo == "almoço e jantar"
+        );
+    }
+
+    // renderiza os cards
+    receitasFiltradas.forEach(receita => {
 
         cards.innerHTML += `
 
